18e9aeb6be10a1f6fdba51026e16d1353a1472de
Ellen <evwhite2@gmail.com>
2020-01-15 18:10:25 -0600

    updating navigation for trips page, added anchor to saved trips page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,27 +2,58 @@
 var db= require("../models"); 
 var express = require("express");
 var router = express.Router();
-const app = express();
 
 var usersAPI = require("./users-api");
 var tripsAPI = require("./trips-api");
 var stopsAPI = require("./stops-api");
 
+const userContent = { userName: '', firstName: '', loggedin: false, body: 'foo'}
 
-router.get("/savedTrips", function(req, res) {
-        db.Trip.findAll({}).then(function(data){
-          tripData = {trip : data}
-          res.render('savedTrips', tripData)
-        })
+
+router.get("/trips", function(req, res) { 
   
+  if (req.session.user && req.cookies.user_sid) {
+		userContent.loggedin = true; 
+    userContent.userName = req.session.user.userName; 
+    userContent.firstName = req.session.user.firstName;
+		userContent.title = "You are logged in"; 
+
+        db.User.findOne({ where: { id: req.session.user.id, include: [db.Trip] } }).then(function(data){
+            var userData= { user : data };
+            res.render('trips', userData);
+          })
+        res.render('trips', userContent);
+    } else {
+        res.redirect('/login');
+    }
   });
 
+ 
+  router.get("/savedTrips", function(req, res) {
+    if (req.session.user && req.cookies.user_sid) {
+      userContent.loggedin = true; 
+      userContent.userName = req.session.user.userName; 
+      userContent.firstName = req.session.user.firstName;
+      userContent.title = "You are logged in"; 
 
-router.get("/trips", function(req, res) {      
-  res.render('trips')
+
+      db.Trip.findAll({
+        where:{
+          UserId: req.session.user.id
+        }
+      }).then(function(data){
+        tripData = {trip : data}
+        console.log("data!!!", data) //this is still not rendering all navigation tabs when user is logged in
+        res.render('savedTrips', tripData)
+      })
+  
+    }else{
+      res.redirect('/login');
+    }
   });
 
   router.use(tripsAPI);
+  
   
   router.get("/interests", function(req, res) {
     res.render('interests')
